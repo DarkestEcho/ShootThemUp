4a39cfbe8e0f3b8f6b9ea4b70a70687c85c3f9bf
Dmitriy Samarin <Samarin99@gmail.com>
2022-12-02 14:25:55 +0100

    Added BaseWeapon

---
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/STUBaseWeapon.h"
+
+
+ASTUBaseWeapon::ASTUBaseWeapon()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+    SetRootComponent(WeaponMesh);
+}
+
+void ASTUBaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
