c12c633d720bcdbfb909524de7993bd9970348cb
Dmitriy Samarin <Samarin99@gmail.com>
2022-12-12 20:25:08 +0100

    Create STUUtils class and template func GetComponentFromPawn

---
+++ Source/ShootThemUp/Public/STUUtils.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+
+class STUUtils
+{
+public:
+    template<class T>
+    static T* GetComponentFromPawn(const APawn* Pawn)
+    {
+        if(!Pawn)
+        {
+            return nullptr;
+        }
+        return Cast<T>(Pawn->GetComponentByClass(T::StaticClass()));
+    }
+};
