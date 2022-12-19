fcbae6785160b1272a0e95efa130fb6440a96378
Dmitriy Samarin <Samarin99@gmail.com>
2022-12-19 13:09:20 +0100

    Create STUBasePickup.cpp

---
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -0,0 +1,37 @@
+#include "Pickups/STUBasePickup.h"
+
+#include "Components/SphereComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogBasePickup, All, All);
+
+ASTUBasePickup::ASTUBasePickup()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+    CollisionComponent = CreateDefaultSubobject<USphereComponent>("CollisionComponent");
+    CollisionComponent->InitSphereRadius(50.0f);
+    CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+    SetRootComponent(CollisionComponent);
+}
+
+void ASTUBasePickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+    Super::NotifyActorBeginOverlap(OtherActor);
+
+    UE_LOG(LogBasePickup, Display, TEXT("Pickup was taken"));
+    Destroy();
+}
+
+void ASTUBasePickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
