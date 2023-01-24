db68e0f38d2ce20f198dfa96141bf64a9a344c69
Dmitriy Samarin <Samarin99@gmail.com>
2023-01-24 20:29:36 +0100

    Added FDecalData and FImpactData

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -3,6 +3,7 @@
 #include "STUCoreTypes.generated.h"
 
 class ASTUBaseWeapon;
+class UNiagaraSystem;
 
 // Weapon
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnClipEmptySignature, ASTUBaseWeapon*);
@@ -58,3 +59,36 @@ struct FWeaponUIData
     UTexture2D* CrossHairIcon;
 };
 #pragma endregion UI
+
+// VFX
+#pragma region VFX
+USTRUCT(BlueprintType)
+struct FDecalData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    UMaterialInterface* Material;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    FVector Size = FVector(10.f);
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    float LifeTime = 5.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    float FadeOutTime = 0.7f;
+};
+
+USTRUCT(BlueprintType)
+struct FImpactData
+{
+    GENERATED_BODY()
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    UNiagaraSystem* NiagaraEffect;
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="VFX")
+    FDecalData DecalData;
+};
+#pragma endregion VFX
