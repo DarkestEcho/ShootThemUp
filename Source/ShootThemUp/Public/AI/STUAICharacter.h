c2790b43ea175ca69b2b7434a7ff7d935c8cbb0d
Dmitriy Samarin <Samarin99@gmail.com>
2023-01-27 17:53:38 +0100

    Create STUAICharacter.h

---
+++ Source/ShootThemUp/Public/AI/STUAICharacter.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/STUBaseCharacter.h"
+#include "STUAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUAICharacter : public ASTUBaseCharacter
+{
+	GENERATED_BODY()
+public:
+    ASTUAICharacter(const FObjectInitializer& ObjInit);
+};
