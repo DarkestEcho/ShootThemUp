6cfe951df52db73dea3bc3e3110314ee7f641729
Dmitriy Samarin <Samarin99@gmail.com>
2023-01-27 17:53:34 +0100

    Create STUAICharacter.cpp

---
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/STUAICharacter.h"
+
+#include "STUAIController.h"
+
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(ObjInit)
+{
+    AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+    AIControllerClass = ASTUAIController::StaticClass();
+}
