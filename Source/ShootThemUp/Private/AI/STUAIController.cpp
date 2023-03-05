097d366738ec1b2454413e0dd033c625efc27f3a
Dmitriy <samarin99@gmail.com>
2023-03-05 16:12:53 +0200

    Setup running BT in controller

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -3,3 +3,14 @@
 
 #include "AI/STUAIController.h"
 
+#include "STUAICharacter.h"
+
+void ASTUAIController::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
+ 
+    if(const ASTUAICharacter* AICharacter = Cast<ASTUAICharacter>(InPawn))
+    {
+        RunBehaviorTree(AICharacter->BehaviorTreeAsset);
+    }
+}
