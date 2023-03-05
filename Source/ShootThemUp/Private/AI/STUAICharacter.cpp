bebdf068ed4d8a67ca1f68a0d387074c53442f3a
Dmitriy <samarin99@gmail.com>
2023-03-05 16:32:18 +0200

    Update STUAICharacter.cpp

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -4,9 +4,18 @@
 #include "AI/STUAICharacter.h"
 
 #include "STUAIController.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(ObjInit)
 {
     AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
     AIControllerClass = ASTUAIController::StaticClass();
+
+    bUseControllerRotationYaw = false;
+
+    if(UCharacterMovementComponent* MovementComponent = GetCharacterMovement())
+    {
+        MovementComponent->bUseControllerDesiredRotation = true;
+        MovementComponent->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
+    }
 }
